7eb01b2f8ca747c53fafa174bb39886822d8360d
Alexandrid998 <71434801+Alexandrid998@users.noreply.github.com>
2022-02-20 20:25:32 +0100

    Update ZeusFit.java

--- src/main/java/ZeusFit/ZeusFit.java
+++ src/main/java/ZeusFit/ZeusFit.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 
 public class ZeusFit {
+    @Value("${dbname}")
+    private String dbname;
 
 	public static void main(String[] args) {SpringApplication.run(ZeusFit.class, args);}
 
